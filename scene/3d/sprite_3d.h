92d64c3c7f77b0eca3070aa07cf23e7b2b181a79
Burak Eberhardt <burak.eberhardt@gmail.com>
2025-05-27 15:47:27 +0200

    Add relative offset for Sprite3D

--- scene/3d/sprite_3d.h
+++ scene/3d/sprite_3d.h
@@ -67,6 +67,7 @@ private:
 
 	bool centered = true;
 	Point2 offset;
+	Point2 relative_offset;
 
 	bool hflip = false;
 	bool vflip = false;
@@ -124,6 +125,9 @@ public:
 
 	void set_offset(const Point2 &p_offset);
 	Point2 get_offset() const;
+
+	void set_relative_offset(const Point2 &p_offset);
+	Point2 get_relative_offset() const;
 
 	void set_flip_h(bool p_flip);
 	bool is_flipped_h() const;
