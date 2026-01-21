74c202c20ca4dae6241e6e8e33b19106c4784f7a
Burak Eberhardt <burak.eberhardt@gmail.com>
2026-01-21 16:22:50 +0100

    Expose all camera attributes during Custom Projection

--- scene/3d/camera_3d.cpp
+++ scene/3d/camera_3d.cpp
@@ -121,11 +121,11 @@ void Camera3D::_update_camera_mode() {
 void Camera3D::_validate_property(PropertyInfo &p_property) const {
 	if (Engine::get_singleton()->is_editor_hint()) {
 		if (p_property.name == "fov") {
-			if (mode != PROJECTION_PERSPECTIVE) {
+			if (mode != PROJECTION_PERSPECTIVE && mode != PROJECTION_CUSTOM) {
 				p_property.usage = PROPERTY_USAGE_NO_EDITOR;
 			}
 		} else if (p_property.name == "size") {
-			if (mode != PROJECTION_ORTHOGONAL && mode != PROJECTION_FRUSTUM) {
+			if (mode != PROJECTION_ORTHOGONAL && mode != PROJECTION_FRUSTUM && mode != PROJECTION_CUSTOM) {
 				p_property.usage = PROPERTY_USAGE_NO_EDITOR;
 			}
 		} else if (p_property.name == "frustum_offset") {
@@ -138,13 +138,13 @@ void Camera3D::_validate_property(PropertyInfo &p_property) const {
 			p_property.usage = PROPERTY_USAGE_NO_EDITOR;
 		}
 	} else if (p_property.name == "near") {
-		if (mode == PROJECTION_CUSTOM) {
-			p_property.usage = PROPERTY_USAGE_NO_EDITOR;
-		}
+		// if (mode == PROJECTION_CUSTOM) {
+		// 	p_property.usage = PROPERTY_USAGE_NO_EDITOR;
+		// }
 	} else if (p_property.name == "far") {
-		if (mode == PROJECTION_CUSTOM) {
-			p_property.usage = PROPERTY_USAGE_NO_EDITOR;
-		}
+		// if (mode == PROJECTION_CUSTOM) {
+		// 	p_property.usage = PROPERTY_USAGE_NO_EDITOR;
+		// }
 	}
 
 	if (attributes.is_valid()) {
